11bf60af97a7a86ea8c7d069a0a91f0a86b9761a
Rodrigo Pablo <rpablo30@hotmail.com>
2022-10-10 19:13:28 -0300

    primeiro commit

---
+++ src/main/java/com/rpablo30/bookstore/BookstoreApplication.java
@@ -0,0 +1,14 @@
+package com.rpablo30.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+		System.out.println("Rodando com sucesso.");
+	}
+
+}
